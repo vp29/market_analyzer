61a0873768ce481964cf35b05b12c3f50a6e7737
AK <shemer77@gmail.com>
2015-01-19 23:36:23 -0500

    moved shit

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-#python setup.py build_ext --inplace
-
-setup(
-  name = 'Least Squares',
-  ext_modules = cythonize("function_script.pyx"),
-)
